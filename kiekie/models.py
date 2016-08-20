e7bb3a7c6a772dd354b095461e00d71dc258f78e
Walter Leibbrandt <git wrl co za>
2016-08-21 01:11:40 +0200

    Expanded picture model with many new fields

--- kiekie/models.py
+++ kiekie/models.py
@@ -1,7 +1,34 @@
+import os
+
+from django.contrib.auth.models import User
 from django.db import models
 from django_extensions.db.fields import UUIDField
 
 
+def pic_upload_path(instance, filename):
+    _, ext = os.path.splitext(filename)
+    return '{0}/{1}{2}'.format(instance.owner.username, instance.id, ext)
+
+
 class Picture(models.Model):
-    uuid = UUIDField(version=4, auto=True, primary_key=True)
+    id = UUIDField(version=4, auto=True, primary_key=True)
+    owner = models.ForeignKey(User, related_name='pictures')
+    file = models.ImageField(upload_to=pic_upload_path)
     note = models.TextField(blank=True, null=True)
+    flagged = models.BooleanField(default=False)
+    num_views = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        flagged, note = '', ''
+        if self.flagged:
+            flagged = '!!'
+        if self.note:
+            note = ' ' + repr(self.note)
+
+        return '{f}{id} #{nviews} {owner} {size}{note}'.format(
+            id=self.id, f=flagged, nviews=self.num_views,
+            owner=self.owner.username, filename=self.file.name,
+            size=self.file.size, note=note)

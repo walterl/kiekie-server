60cf1070a09b8b68606929855cae2a378cdeeb5c
Walter Leibbrandt <git wrl co za>
2016-08-21 01:37:04 +0200

    Exclude 'file' and 'owner' fields, add custom hyperlinked 'download' field

--- kiekie/api/serializers.py
+++ kiekie/api/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer, HyperlinkedIdentityField)
 
 from kiekie.models import Picture
 
@@ -6,3 +7,6 @@ from kiekie.models import Picture
 class PictureSerializer(ModelSerializer):
     class Meta:
         model = Picture
+        exclude = ('file', 'owner')
+
+    download = HyperlinkedIdentityField(view_name='Picture-download')

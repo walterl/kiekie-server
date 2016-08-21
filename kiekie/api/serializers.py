cd8dd4693d2d3c79965e6870d9da33b2c693952f
Walter Leibbrandt <git wrl co za>
2016-08-21 03:10:24 +0200

    Further don't emit 'flagged' or 'num_views' fields from API

--- kiekie/api/serializers.py
+++ kiekie/api/serializers.py
@@ -7,6 +7,6 @@ from kiekie.models import Picture
 class PictureSerializer(ModelSerializer):
     class Meta:
         model = Picture
-        exclude = ('file', 'owner')
+        exclude = ('file', 'owner', 'flagged', 'num_views')
 
     download = HyperlinkedIdentityField(view_name='Picture-download')
